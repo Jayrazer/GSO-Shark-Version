f072128817ddcc6e32e524b63aa4967ac5a6857b
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-04-24 12:15:32 -0400

    Update go_m1014_mag_7.lua

--- lua/arccw/shared/attachments/go_m1014_mag_7.lua
+++ lua/arccw/shared/attachments/go_m1014_mag_7.lua
@@ -1,4 +1,4 @@
-att.PrintName = "7-Round M1014 Tube"
+att.PrintName = "8-Round M1014 Tube"
 att.Icon = Material("entities/acwatt_go_m1014_mag_7.png", "mips smooth")
 att.Description = "Extended magazine with 7-round capacity."
 att.SortOrder = 7
@@ -10,7 +10,7 @@ att.Desc_Cons = {
 att.AutoStats = true
 att.Slot = "go_m1014_mag"
 
-att.Mult_MoveSpeed = 0.98
+att.Mult_MoveSpeed = 0.97
 att.Mult_SightTime = 1.05
-att.Override_ClipSize = 7
-att.Mult_ReloadTime = 1.1
+att.Override_ClipSize = 8
+att.Mult_ReloadTime = 1.15
